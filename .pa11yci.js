14f8cc40171e5dcdbdd50765a4659873b21b81d5
Oleksii Didenko <oleksii.did.it@gmail.com>
2025-05-29 17:19:03 +0100

    fix: try to run pa11y

---
+++ .pa11yci.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const urlsFilePath = path.resolve(__dirname, 'urls.txt');
+
+const urls = fs.readFileSync(urlsFilePath, 'utf8')
+    .split('\n')
+    .map(url => url.trim())
+    .filter(url => url.length > 0);
+
+const REPORT_DATE = Date.now();
+
+module.exports = {
+    urls,
+    reporters: [
+        { name: "json", filename: `./public/pa11y/${REPORT_DATE}/pa11y.json` },
+        { name: "pa11y-ci-reporter-html", filename: `./public/pa11y/${REPORT_DATE}/index.html` }
+    ],
+    defaults: {
+        timeout: 60000,
+        chromeLaunchConfig: {
+            executablePath: process.env.GITHUB_ACTIONS === 'true'
+                ? '/usr/bin/google-chrome'
+                : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+            args: [
+                "--no-sandbox"
+            ]
+        },
+    },
+};
