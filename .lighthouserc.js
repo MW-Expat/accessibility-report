4a4827e1af95d549e7b09c47c2c397cfd30f397e
Oleksii Didenko <oleksii.did.it@gmail.com>
2025-05-30 16:44:36 +0100

    fix: try to run lighthouse

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
 const urls = fs.readFileSync('urls.txt', 'utf-8')
     .split('\n')
-    .filter(Boolean);
+    .map(url => url.trim())
+    .filter(url => url.length > 0);
 
 module.exports = {
     ci: {
         collect: {
-            url: urls,
-            numberOfRuns: 1
+            url: urls[0],
+            numberOfRuns: 1,
+            chromePath: process.env.GITHUB_ACTIONS === 'true'
+                ? '/usr/bin/google-chrome'
+                : '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
         },
         upload: {
             target: 'filesystem', // write HTML locally
             outputDir: `./public/lhci/${process.env.REPORT_DATE}`, // dated folder
-            reportFilenamePattern: '[name]-report.html'
+            reportFilenamePattern: 'index.html'
         },
         assert: { preset: 'lighthouse:no-pwa' } // skip the PWA category
     }
